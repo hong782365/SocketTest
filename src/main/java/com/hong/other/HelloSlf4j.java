8ccaf7e5b629cffffb20f9b7b766ac11e1480114
derek <hong782365@sina.com>
2017-03-02 22:21:59 +0800

    修改 build.gradle 配置

--- src/main/java/com/hong/other/HelloSlf4j.java
+++ src/main/java/com/hong/other/HelloSlf4j.java
@@ -1,5 +1,7 @@
 package com.hong.other;
 
+
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,9 +13,18 @@ public class HelloSlf4j {
     private static final Logger logger = LoggerFactory.getLogger(HelloSlf4j.class);
 
     public static void main(String[] args) {
+        System.out.println(111);
         logger.debug("This is debug message");
         logger.info("This is info message");
         logger.warn("This is warn message");
         logger.error("This is error message");
+        System.out.println(222);
+    }
+
+    @Test
+    public void test(){
+        System.out.println(333);
+        logger.info("test info");
+        System.out.println(444);
     }
 }
---
+++ src/main/java/com/hong/other/MainTest.java
@@ -0,0 +1,11 @@
+package com.hong.other;
+
+/**
+ * Created by wangweihong on 17/3/2.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("test main");
+    }
+}
